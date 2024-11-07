403254d0587b6853124fb8e2b69b4d5b5ff8dc7d
Eren <114621741+k-eren-k@users.noreply.github.com>
2024-11-07 23:13:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const fs= require('fs');
+const ejs = require('ejs');
+const path = require('path');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const User = require('./models/User');
@@ -15,7 +18,10 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret: 'secretkey',
